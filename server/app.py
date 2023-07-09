253f7c316afa8a87c026d20b3dcfa7df4265098d
rebeccanakhanu <rebeccanakhanu@gmail.com>
2023-07-10 00:17:21 +0300

    completed assignments

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -10,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json_encoder.compact = False
 
 migrate = Migrate(app, db)
 
@@ -18,40 +16,34 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class ClearSession(Resource):
-
     def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
+        session.pop('user_id', None)
         return {}, 204
 
+
 class IndexArticle(Resource):
-    
     def get(self):
         articles = [article.to_dict() for article in Article.query.all()]
         return articles, 200
 
-class ShowArticle(Resource):
 
+class ShowArticle(Resource):
     def get(self, id):
-        session['page_views'] = 0 if not session.get('page_views') else session.get('page_views')
-        session['page_views'] += 1
+        session['page_views'] = session.get('page_views', 0) + 1
 
         if session['page_views'] <= 3:
-
             article = Article.query.filter(Article.id == id).first()
-            article_json = jsonify(article.to_dict())
-
-            return make_response(article_json, 200)
+            article_json = make_response(jsonify(article.to_dict()), 200)
+            return article_json
 
         return {'message': 'Maximum pageview limit reached'}, 401
+
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
